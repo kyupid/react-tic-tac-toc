110d9eb3f533e309395ea781679567e2a7d193e7
Yeonwoo Kim <kywoopid@gmail.com>
2024-05-15 16:19:10 +0900

    상태값들을 최상단 컴포넌트인 Game 에서 관리하도록 수정

--- src/App.js
+++ src/App.js
@@ -8,10 +8,7 @@ function Square({value, onSquareClick}) {
     );
 }
 
-export default function Board() {
-    const [xIsNext, setXIsNext] = useState(true);
-    const [squares, setSquares] = useState(Array(9).fill(null));
-
+function Board({xIsNext, squares, onPlay}) {
     function handleClick(i) {
         if (squares[i] || calculateWinner(squares)) {
             return;
@@ -53,6 +50,27 @@ export default function Board() {
                 <Square value={squares[8]} onSquareClick={() => handleClick(8)}/>
             </div>
         </>
+    );
+}
+
+export default function Game() {
+    const [xIsNext, setXIsNext] = useState(true);
+    const [squares, setSquares] = useState(Array(9).fill(null));
+    const currentSquares = history[history.length - 1];
+
+    function handlePlay(nextSquares) {
+        // TODO
+    }
+
+    return (
+        <div className="game">
+            <div className="game-board">
+                <Board xIsNext={xIsNext} squares={currentSquares} onPlay={handlePlay}/>
+            </div>
+            <div className="game-info">
+                <ol>{/*TODO*/}</ol>
+            </div>
+        </div>
     );
 }
 
